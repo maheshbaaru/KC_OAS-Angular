723c7344f502a160fc0fc28b597814aa9a4023f4
maheshbaaru <mahebaaru@gmail.com>
2023-05-02 18:00:16 +0530

    methods

--- src/app/profilecomponents/updateprofile/updateprofile.component.ts
+++ src/app/profilecomponents/updateprofile/updateprofile.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { EmployeedDataService } from 'src/app/services/EmployeesDataService';
+import { HttpClientService } from 'src/app/services/http-client.service';
 
 
 @Component({
@@ -7,7 +10,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./updateprofile.component.css']
 })
 export class UpdateprofileComponent {
+  empdata: any;
+  dendata:any
+  constructor(
+  private router:Router,
+    private service: EmployeedDataService,private designatonservice:HttpClientService
+  ) { }
 
-  
+  ngOnInit(): void {
+   this.formdataget()
+  }
+
+
+  formdataget(){
+    this.service.getEmployeeList().subscribe((data1: any) => {
+      console.log(data1);
+            this.empdata = data1;
+
+    });
+  }
+
+
+  formdatpost(){
+ this.designatonservice.postdesignation((data:any) =>{
+      console.log(data)
+      this. dendata= data;
+    });
+  }
 
 }
