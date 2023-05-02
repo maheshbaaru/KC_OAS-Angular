0a975bd7b77c24916dd7187db18343f98b7740d2
mvaleru <mvaleru@kodecorp.com>
2023-05-02 18:46:45 +0530

    changes in create new salary component

--- src/app/routing/create-new-salary-details/create-new-salary-details.component.ts
+++ src/app/routing/create-new-salary-details/create-new-salary-details.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+//import { EmployeedDataService } from 'src/app/services/EmployeesDataService';
 import namesData from 'src/assets/data/names.json';
 interface EmployeeName {
   Name: string
@@ -14,23 +15,32 @@ interface EmployeeName {
 export class CreateNewSalaryDetailsComponent {
   names: EmployeeName[];
   empForm: FormGroup;
-  
-  
-constructor(private _fb:FormBuilder){
-  this.names = namesData
-  this.empForm=this._fb.group({
-    name:'',
-    Salary:'',
-    LastRevisedDate:'',
-    NextRevisedDate:''
-  })
-  
-}
 
-onFormSubmit(){
-  if(this.empForm.valid){
-    console.log(this.empForm.value)
+  constructor(private _fb: FormBuilder, 
+    //private _service: EmployeedDataService
+    ) {
+    this.names = namesData
+    this.empForm = this._fb.group({
+      name: '',
+      Salary: '',
+      LastRevisedDate: '',
+      NextRevisedDate: ''
+    })
+
   }
-}
 
+  onFormSubmit() {
+    if (this.empForm.valid) {
+      console.log(this.empForm.value)
+    }
+  }
+  // ngOnInit() {
+  //   this._service.getEmployeeList().subscribe(data => {
+  //     console.log(data);
+  //     this.names = data.filter((item: any) => {
+  //       return item.firstName + item.lastName;
+  //       console.log(this.names);
+  //     })
+  //   });
+  // }
 }
