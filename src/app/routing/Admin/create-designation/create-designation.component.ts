80adde872f61412b69c1ef51e997b19d043fb784
apsar-sk <sapsar@kodecorp.com>
2023-05-08 09:29:53 +0530

    create designation post API(apsar).

--- src/app/routing/Admin/create-designation/create-designation.component.ts
+++ src/app/routing/Admin/create-designation/create-designation.component.ts
@@ -1,25 +1,35 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { EmployeedDataService } from 'src/app/services/EmployeesDataService';
 import { HttpClientService } from 'src/app/services/http-client.service';
+import { SalarydeductionlistService } from 'src/app/services/salarydeductionlist.service';
 
 @Component({
   selector: 'app-create-designation',
   templateUrl: './create-designation.component.html',
-  styleUrls: ['./create-designation.component.css']
+  styleUrls: ['./create-designation.component.css'],
 })
 export class CreateDesignationComponent {
-
+  addDesination: any;
   constructor(
-    private router:Router,
+    fb: FormBuilder,
+    private router: Router,
     private service: EmployeedDataService,
-    private dgnservice:HttpClientService,
-    private formBuilder: FormBuilder
-  ) { }
-
-  submit(){
- 
+    private dgnservice: HttpClientService,
+    private formBuilder: FormBuilder,
+    private salDeductionServe: SalarydeductionlistService
+  ) {
+    this.form = fb.group({
+      name: [null, Validators.required],
+    });
   }
- 
+
+  submit() {}
+  public form: FormGroup;
+  addDesignation(name: any) {
+    this.salDeductionServe.AddDesignation(name).subscribe((res) => {
+      this.addDesination = res;
+    });
+  }
 }
--- src/app/services/salarydeductionlist.service.ts
+++ src/app/services/salarydeductionlist.service.ts
@@ -1,16 +1,30 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SalarydeductionlistService {
   API_CALL = 'https://localhost:7236';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
   constructor(private http: HttpClient) {}
-  ngOnInit() {
-  }
-  getTaxDeduction(){
+  ngOnInit() {}
+  getTaxDeduction() {
     return this.http.get(`${this.API_CALL}/TblTaxDeduction`);
-
+  }
+  AddDesignation(name: string) {
+    debugger;
+    return new Observable<any[]>((observer) => {
+      this.http.post<any>(
+        `${this.API_CALL}/AddDesignations?Name=`,
+        JSON.stringify({ name }),
+        this.httpOptions
+      );
+    });
   }
 }
