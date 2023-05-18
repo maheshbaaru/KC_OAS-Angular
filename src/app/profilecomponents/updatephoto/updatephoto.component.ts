b9d05ee69c9f4243394579eac5645cda3173a6b5
maheshbaaru <mahebaaru@gmail.com>
2023-05-18 09:28:20 +0530

    updatecode arrangement

--- src/app/profilecomponents/updatephoto/updatephoto.component.ts
+++ src/app/profilecomponents/updatephoto/updatephoto.component.ts
@@ -108,67 +108,6 @@ export class UpdatephotoComponent {
   }
   }
 
+ 
 
-  selectFile(event: any): void {
-    this.message = '';
-    this.preview = '';
-    this.progress = 0;
-    this.selectedFiles = event.target.files;
-    if (this.selectedFiles) {
-      const file: File | null = this.selectedFiles.item(0);
-      if (file) {
-        this.preview = '';
-        this.data = file;
-        const reader = new FileReader();
-        reader.onload = (e: any) => {
-          this.preview = e.target.result;
-        };
-        reader.readAsDataURL(this.data);
-      }
-    }
-  }
-
-  // upload(): void {
-  //   this.progress = 0;
-
-  //   if (this.selectedFiles) {
-  //     const file: File | null = this.selectedFiles.item(0);
-
-  //     if (file) {
-  //       this.currentFile = file;
-
-  //       console.log(this.currentFile);
-
-  //       this.profileServ
-  //         .addprofilephoto({
-  //           EmpId: this.empdata.employeeID,
-  //           Photo: this.preview,
-  //         })
-  //         .subscribe({
-  //           next: (event: any) => {
-  //             // if (event.type === HttpEventType.UploadProgress) {
-  //             //   this.progress = Math.round((100 * event.loaded) / event.total);
-  //             // } else if (event instanceof HttpResponse) {
-  //             //   this.message = event.body.message;
-  //             //   this.imageInfos = this.profileServ.addprofilephoto(EmpId,Id);
-  //             // }
-  //           },
-  //           error: (err: any) => {
-  //             console.log(err);
-  //             this.progress = 0;
-
-  //             if (err.error && err.error.message) {
-  //               this.message = err.error.message;
-  //             } else {
-  //               this.message = 'Could not upload the image!';
-  //             }
-
-  //             this.currentFile = undefined;
-  //           },
-  //         });
-  //     }
-
-  //     this.selectedFiles = undefined;
-  //   }
-  // }
 }
