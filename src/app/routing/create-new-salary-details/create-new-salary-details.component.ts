eba0b1f3d4255736f7fa76319d96a126e76bd111
apsar-sk <sapsar@kodecorp.com>
2023-05-21 12:25:21 +0530

    Create salry validations(apsar).

--- src/app/routing/create-new-salary-details/create-new-salary-details.component.ts
+++ src/app/routing/create-new-salary-details/create-new-salary-details.component.ts
@@ -1,9 +1,15 @@
 import { formatDate } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { EmployeedDataService } from 'src/app/services/EmployeesDataService';
 import { Inject, LOCALE_ID } from '@angular/core';
+import { MessageService } from 'primeng/api';
 interface EmployeeName {
   Name: string;
 }
@@ -13,7 +19,6 @@ interface EmployeeName {
   templateUrl: './create-new-salary-details.component.html',
   styleUrls: ['./create-new-salary-details.component.css'],
 })
-
 export class CreateNewSalaryDetailsComponent {
   names: EmployeeName[];
   empForm: FormGroup;
@@ -29,21 +34,18 @@ export class CreateNewSalaryDetailsComponent {
     private _fb: FormBuilder,
     private _service: EmployeedDataService,
     private router: Router,
-    @Inject(LOCALE_ID) public local: string,
-  ) {
-    
-  }
-
+    private messageService: MessageService,
+    @Inject(LOCALE_ID) public local: string
+  ) {}
 
   ngOnInit() {
     this.empForm = this._fb.group({
       EmpId: [null, Validators.required],
-    
-      Salary: [null, Validators.required],
-      LastRevisedDate:  [null, Validators.required],
-      NextRevisedDate:  [null, Validators.required],
-    });
 
+      Salary: [null, Validators.required],
+      LastRevisedDate: [null, Validators.required],
+      NextRevisedDate: [null, Validators.required],
+    });
 
     this._service.getEmployeeList().subscribe((data) => {
       this.names = data.filter((item: any) => {
@@ -52,29 +54,51 @@ export class CreateNewSalaryDetailsComponent {
     });
   }
 
-  get f() { return this.empForm.controls; }
-
+  get f() {
+    return this.empForm.controls;
+  }
 
   onFormSubmit() {
-    this.submitted=true;
-    this .EmpId=this.empForm.value.EmpId.employeeID;
-    this.Eid=this.EmpId.replace('KC0',"");
-    
-    console.log(this.Eid);
-        this.Salary = this.empForm.value.Salary;
-        (this.LastRevisedDate  = formatDate(this.empForm.value.LastRevisedDate, 'YYYY-MM-dd', this.local)),
-        (this.NextRevisedDate  = formatDate(this.empForm.value.NextRevisedDate, 'YYYY-MM-dd', this.local))
-        this._service.CreatenewSalaryDetails(this.Eid, this.Salary,this.LastRevisedDate,this.NextRevisedDate);
-    
-        if (this.empForm.valid) {
-          this._service.CreatenewSalaryDetails(this.Eid, this.Salary,this.LastRevisedDate,this.NextRevisedDate).subscribe(res=>{
-            console.log(res)
-          });
-        }
-        this.empForm.reset();
+    this.submitted = true;
+    if (this.empForm.invalid) {
+      for (const control of Object.keys(this.empForm.controls)) {
+        this.empForm.controls[control].markAsTouched();
+        this.empForm.controls[control].markAsDirty();
       }
-    
- 
-  
-  
+      return;
+    } else if (this.empForm.valid) {
+      this.EmpId = this.empForm.value.EmpId.employeeID;
+      this.Eid = this.EmpId.replace('KC0', '');
+
+      this.Salary = this.empForm.value.Salary;
+      (this.LastRevisedDate = formatDate(
+        this.empForm.value.LastRevisedDate,
+        'YYYY-MM-dd',
+        this.local
+      )),
+        (this.NextRevisedDate = formatDate(
+          this.empForm.value.NextRevisedDate,
+          'YYYY-MM-dd',
+          this.local
+        ));
+
+      this._service
+        .CreatenewSalaryDetails(
+          this.Eid,
+          this.Salary,
+          this.LastRevisedDate,
+          this.NextRevisedDate
+        )
+        .subscribe((res) => {
+          if (res) {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Success',
+              detail: 'TaxType saved',
+            });
+          }
+        });
+    }
+    this.empForm.reset();
+  }
 }
