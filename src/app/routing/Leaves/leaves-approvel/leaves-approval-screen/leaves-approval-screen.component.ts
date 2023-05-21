b7843c433d6f2684a92f7e44fe188db8418c97c3
saleemjavid360 <msaleem@kodecorp.com>
2023-05-21 14:42:16 +0530

    create,update,leaves,validations

--- src/app/routing/Leaves/leaves-approvel/leaves-approval-screen/leaves-approval-screen.component.ts
+++ src/app/routing/Leaves/leaves-approvel/leaves-approval-screen/leaves-approval-screen.component.ts
@@ -1,8 +1,15 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EmployeeService } from 'src/app/services/employee.service';
 import { LeavesService } from 'src/app/services/leaves.service';
 
+interface statustype {
+  statusType: string;
+}
+interface leavetype {
+  leaveTypeName: string;
+}
 export interface EmployeeInterFace{
   adminComments?:string,
   appliedOn ?:string,
@@ -24,11 +31,51 @@ export interface EmployeeInterFace{
   toDate   ?:string,
 }
 
+const shakeAnimation = trigger('shakeAnimation', [
+  state('invalid', style({
+    transform: 'translateX(0px)',
+    borderColor: 'red'
+  })),
+  state('invalid', style({
+    transform: 'translateX(0px)',
+  })),
+  transition('* => invalid', [
+    animate('0.1s', style({ transform: 'translateX(10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(-10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(-10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(-10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(-10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(0px)', borderColor: 'red' })),
+  ]),
+  transition('invalid => *', [
+    animate('0.1s', style({ transform: 'translateX(0px)'})),
+  ])
+]);
+
+
+const shakeAnimationPassword =trigger('shakeAnimationPassword',[
+  state('invalid',style({
+    transform:'translateX(0px)',
+    borderColor:'red'
+  })),
+  
+  
+  transition('* => invalid', [
+    animate('0.1s', style({ transform: 'translateX(10px)' , borderColor:'red'})),
+    animate('0.1s', style({ transform: 'translateX(-10px)', borderColor:'red' })),
+    animate('0.1s', style({ transform: 'translateX(0px)', borderColor:'red' }))
+  ]),
+])
 
 @Component({
   selector: 'leaves-approval-screen',
   templateUrl: './leaves-approval-screen.component.html',
   // styleUrls: ['./leaves-approval-screen.component.css']
+  animations:[shakeAnimation,shakeAnimationPassword],
   styleUrls: ['./leaves-approval-screen.component.scss']
 })
 export class LeavesApprovalScreenComponent {
@@ -42,8 +89,24 @@ export class LeavesApprovalScreenComponent {
 
   leaveTypeName="Sick";
   statusIdName="Approved";
+  // isSubmited=true
+  isSubmited=false
   
+  leaveTypeName2: leavetype;
+  statusTypeName2: statustype;
+  // ngDoCheck(){
+  //   console.log(this.leaveTypeName)
+  // }
+  
+
   ngOnInit() {
+
+    const toast = document.getElementById("success-toast") as HTMLElement;
+    if (toast) {
+      toast.textContent = ""; 
+      toast.style.display = "none";
+    }
+
     this.activeRoutIdFunction()
       this.empservice.getSpecifiEmployeeLeavesDataById().subscribe((result1:any)=>{
         for (const result of result1){
@@ -62,6 +125,7 @@ export class LeavesApprovalScreenComponent {
             lastName:result.lastName,
             leaveTypeName:result.leaveTypeName,
             statusTypeName:result.statusTypeName,
+            name:`${result.firstName} ${result.lastName}`
           };
         }
         
@@ -76,12 +140,21 @@ export class LeavesApprovalScreenComponent {
   }
 
   onSelectLeaveTye(event:any){
-    this.leaveTypeName = event.value
+    this.leaveTypeName2 = event
+    this.leaveTypeName = event.leaveTypeName
+   
+  }
+  
+  onSelectStatusType(event:any){
+    this.statusTypeName2 = event
+    this.statusIdName = event.statusType
   }
 
-  onSelectStatusType(event:any){
-    this.statusIdName = event.value
+
+  isLeaveTypeRequired(): boolean {
+    return true; 
   }
+
   speficEmployeeLeaveData={
     adminComments:"",
     appliedOn :"",
@@ -97,6 +170,7 @@ export class LeavesApprovalScreenComponent {
     lastName:"",
     leaveTypeName:"",
     statusTypeName:"",
+    name:""
   } 
   
   statusId=[
@@ -106,69 +180,112 @@ export class LeavesApprovalScreenComponent {
     {statusType:"Forwarded"},
   ]
     
+  // ngDoCheck(){
+  //   console.log(this.speficEmployeeLeaveData)
+  // }
   leaveType=[
-    {leaveTypeName:"Sick"},
     {leaveTypeName:"Casual"},
+    {leaveTypeName:"Sick"},
     {leaveTypeName:"Earned"},
     {leaveTypeName:"Compansation"},
     {leaveTypeName:"Optional"},
   ]
+  showSuccessToast(event:any) {
+    const toast = document.getElementById("success-toast") || null;
+    if (toast) {
+      toast.textContent = "Success!";
+      toast.style.display = "block";
+      toast.style.position = "fixed";
+      toast.style.top = "10px";
+      toast.style.right = "10px";
+      toast.classList.add("toast-appear");
+      setTimeout(() => {
+        toast.classList.remove("toast-appear");
+        toast.style.display = "none";
+      }, 2000);
+    }
+  }
+
+  onSave(event:any){
+    if(event){
+      this.isSubmited=true
+      let leaveTypeid= null
+      let statusId= null
   
-  onSave(){
+        switch(this.leaveTypeName){
+          case "Casual" :
+            leaveTypeid=1
+            break
+          case "Sick":
+            leaveTypeid=2
+            break
+          case "Earned":
+            leaveTypeid=3
+            break
+          case "Compansation":
+            leaveTypeid=4
+            break
+          case "Optional":
+            leaveTypeid=5
+            break
+        }
+  
+  
+        switch(this.statusIdName){
+          case "Approved" :
+            statusId=1
+            break
+          case "New":
+            statusId=2
+            break
+          case "Rejected":
+            statusId=3
+            break
+          case "Forwarded":
+            statusId=4
+            break
+        }
+  
+        const postData={
+          statusId:statusId,
+          leaveTypeId:leaveTypeid,
+          id:this.speficEmployeeLeaveData.id,
+          
+        }
+        this.empservice.updateLeavesApprovalData(postData)
 
+        if(event){
+          this.showSuccessToast(event);
+          }else{
+          const successToast = document.getElementById("success-toast");
+          if (successToast) {
+            successToast.style.display = "none";
+            successToast.style.backgroundColor = "none";
+          }
+        }
+    }
+    else{
+      this.isSubmited=false
+      setTimeout(() => {
+        this.isSubmited=true
+      }, 400);
+      // console.log("form not valid")
+      // console.log(event)
+    }
+    // console.log(typeof(this.statusIdName),this.statusIdName)
     
-    let leaveTypeid= 2
-    let statusId= 1
-
-      switch(this.leaveTypeName){
-        case "Sick":
-          leaveTypeid=2
-          break
-        case "Casual" :
-          leaveTypeid=1
-          break
-        case "Earned":
-          leaveTypeid=3
-          break
-        case "Compensation":
-          leaveTypeid=4
-          break
-        case "Optional":
-          leaveTypeid=5
-          break
-      }
+      // console.log(postData)
+      // console.log(this.speficEmployeeLeaveData.id)
+     
+     
+    }
+  }
 
 
-      switch(this.statusIdName){
-        case "Approved" :
-          statusId=1
-          break
-        case "New":
-          statusId=2
-          break
-        case "Rejected":
-          statusId=3
-          break
-        case "Forwarded":
-          statusId=4
-          break
-      }
-
-      const postData={
-        statusId:statusId,
-        leaveTypeId:leaveTypeid,
-        id:this.speficEmployeeLeaveData.id,
-        // adminComments:this.speficEmployeeLeaveData.adminComments,
+ // adminComments:this.speficEmployeeLeaveData.adminComments,
         // appliedOn:this.speficEmployeeLeaveData.,
         // comments:this.speficEmployeeLeaveData.,
         // empId:this.speficEmployeeLeaveData.,
         // fromDate:this.speficEmployeeLeaveData.,
         // numOfDays:this.speficEmployeeLeaveData.,
         // toDate:this.speficEmployeeLeaveData.,
-      }
-      // console.log(postData)
-      this.empservice.updateLeavesApprovalData(postData)
-    }
-  }
-
-
--- src/app/routing/edit-bank-details/edit-bank-details.component.ts
+++ src/app/routing/edit-bank-details/edit-bank-details.component.ts
@@ -1,5 +1,6 @@
 
 
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EmployeService } from 'src/app/services/employeBankService';
@@ -15,10 +16,50 @@ export interface EmployeeInterFace{
   lastName?:string
 }
 
+const shakeAnimation = trigger('shakeAnimation', [
+  state('invalid', style({
+    transform: 'translateX(0px)',
+    borderColor: 'red'
+  })),
+  state('invalid', style({
+    transform: 'translateX(0px)',
+  })),
+  transition('* => invalid', [
+    animate('0.1s', style({ transform: 'translateX(10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(-10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(-10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(-10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(-10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(10px)', borderColor: 'red' })),
+    animate('0.1s', style({ transform: 'translateX(0px)', borderColor: 'red' })),
+  ]),
+  transition('invalid => *', [
+    animate('0.1s', style({ transform: 'translateX(0px)'})),
+  ])
+]);
+
+
+const shakeAnimationPassword =trigger('shakeAnimationPassword',[
+  state('invalid',style({
+    transform:'translateX(0px)',
+    borderColor:'red'
+  })),
+  
+  
+  transition('* => invalid', [
+    animate('0.1s', style({ transform: 'translateX(10px)' , borderColor:'red'})),
+    animate('0.1s', style({ transform: 'translateX(-10px)', borderColor:'red' })),
+    animate('0.1s', style({ transform: 'translateX(0px)', borderColor:'red' }))
+  ]),
+])
 
 @Component({
   selector: 'edit-bank-details',
   templateUrl: './edit-bank-details.component.html',
+  animations:[shakeAnimation,shakeAnimationPassword],
   styleUrls: ['./edit-bank-details.component.css']
 })
 export class EditBankDetailsComponent implements OnInit{
@@ -31,9 +72,19 @@ export class EditBankDetailsComponent implements OnInit{
 
     employeess: EmployeeInterFace[] | any;
     employeeNameArray:any
+    activedRouteId: string | null;
+    employees: any;
 
+    submited=false
+
+    integerRegex=/^(?:-(?:[1-9](?:\d{0,2}(?:,\d{3})+|\d*))|(?:0|(?:[1-9](?:\d{0,2}(?:,\d{3})+|\d*))))(?:.\d+|)$/
   ngOnInit(): void {
-  
+    const toast = document.getElementById("success-toast") as HTMLElement;
+    if (toast) {
+      toast.textContent = ""; 
+      toast.style.display = "none";
+    }
+
     this.activeRoutIdFunction()
       this.employeeService.getSpecifiEmployeeDataById().subscribe((result:any)=>{
         this.employeess=result
@@ -47,22 +98,59 @@ export class EditBankDetailsComponent implements OnInit{
     })
 
   }
-  activedRouteId: string | null;
-  employees: any;
   activeRoutIdFunction(){
     const activatedRouteId = this.route.snapshot.paramMap.get('id')
     return this.employeeService.id(activatedRouteId)
   }
 
-  onClickSave(){
-    const postData={
-      id:this.employeeNameArray.id,
-      empId:this.employeeNameArray.empId,
-      accno:this.employeeNameArray.accno,
-      bankName:this.employeeNameArray.bankName,
-      accname:this.employeeNameArray.accname,
-      
+
+  showSuccessToast(event:any) {
+    const toast = document.getElementById("success-toast") || null;
+    if (toast) {
+      toast.textContent = "Success!";
+      toast.style.display = "block";
+      toast.style.position = "fixed";
+      toast.style.top = "10px";
+      toast.style.right = "10px";
+      toast.classList.add("toast-appear");
+      setTimeout(() => {
+        toast.classList.remove("toast-appear");
+        toast.style.display = "none";
+      }, 2000);
     }
-   this.employeeService.updateEmployeeBankData(postData)
+  }
+  
+  onClickSave(event:any){
+    if(event){
+      this.submited=true
+      console.log("this valid")
+      const postData={
+        id:this.employeeNameArray.id,
+        empId:this.employeeNameArray.empId,
+        accno:this.employeeNameArray.accno,
+        bankName:this.employeeNameArray.bankName,
+        accname:this.employeeNameArray.accname,
+        
+      }
+     this.employeeService.updateEmployeeBankData(postData)
+    
+     if(event){
+      this.showSuccessToast(event);
+      }else{
+      const successToast = document.getElementById("success-toast");
+      if (successToast) {
+        successToast.style.display = "none";
+        successToast.style.backgroundColor = "none";
+      }
+    }
+
+    }else{
+      console.log("valid")
+      this.submited=false
+      setTimeout(() => {
+        this.submited=true
+      }, 400);
+    }
+   
   }
 }
