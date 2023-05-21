6e92859671fb1c687c958c2a9b45491ec8ad3996
apsar-sk <sapsar@kodecorp.com>
2023-05-21 15:15:39 +0530

    payslip Validations(apsar).

--- src/app/routing/pay-slip/pay-slip.component.ts
+++ src/app/routing/pay-slip/pay-slip.component.ts
@@ -1,24 +1,39 @@
-import { Component } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-pay-slip',
   templateUrl: './pay-slip.component.html',
-  styleUrls: ['./pay-slip.component.css']
+  styleUrls: ['./pay-slip.component.css'],
 })
 export class PaySlipComponent {
-date!:Date;
-constructor(){
- 
-}
-ngOnInit(){
+  myReactiveForm: FormGroup;
+  date!: Date;
+  paySlip: any;
+  constructor(@Inject(LOCALE_ID) public local: string) {}
+  ngOnInit() {
+    this.myReactiveForm = new FormGroup({
+      payslip: new FormControl(null, Validators.required),
+    });
+  }
+  Alert() {
+    if (this.myReactiveForm.invalid) {
+      for (const control of Object.keys(this.myReactiveForm.controls)) {
+        this.myReactiveForm.controls[control].markAsTouched();
+        this.myReactiveForm.controls[control].markAsDirty();
+      }
+      return;
+    } else if (this.myReactiveForm.valid) {
+      this.paySlip = formatDate(
+        this.myReactiveForm.value.payslip,
+        'YYYY-MM',
+        this.local
+      );
+      //api call here
+      alert('Employee does not have bank account or salary');
+    }
 
-}
-Alert(){
-  if(!this.date){
-    alert('please select month and year');
+    this.myReactiveForm.reset();
   }
-  else{
-    alert('Employee does not have bank account or salary');
-  }
-}
 }
