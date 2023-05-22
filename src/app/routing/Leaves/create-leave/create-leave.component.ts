13889c6b806d51fc4a7d7c284c0d049149660b28
apsar-sk <sapsar@kodecorp.com>
2023-05-22 17:57:32 +0530

    create leave sal-deduction,employeeLaves(apsar).

--- src/app/routing/Leaves/create-leave/create-leave.component.ts
+++ src/app/routing/Leaves/create-leave/create-leave.component.ts
@@ -1,14 +1,8 @@
 import { Component, Inject, LOCALE_ID } from '@angular/core';
-import {
-  AbstractControl,
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
-import { DatePipe, formatDate } from '@angular/common';
+import { formatDate } from '@angular/common';
 
 import { LeavesService } from 'src/app/services/leaves.service';
 interface LeaveType {
@@ -31,7 +25,6 @@ export class CreateLeaveComponent {
   id: any;
   submitted = false;
   isLoggedIn = false;
- 
 
   constructor(
     private _fb: FormBuilder,
@@ -52,75 +45,69 @@ export class CreateLeaveComponent {
       .subscribe((res) => (this.leaveTypes = res));
 
     this.empForm = this.formBuilder.group({
-      leaveTypeId: [
-        new FormControl({
-          value: '',
-          disabled: false,
-        }),
-        Validators.required,
-      ],
+      leaveTypeId: [null, Validators.required],
       FromDate: [null, Validators.required],
       ToDate: [null, Validators.required],
-      StatusId:[null,Validators.required],
-      Comments:['', Validators.required],
+      StatusId: [null, Validators.required],
+      Comments: ['', Validators.required],
     });
-  }
-  get f(): { [key: string]: AbstractControl } {
-    return this.empForm.controls;
   }
 
   onFormSubmit() {
     if (this.empForm.invalid) {
       for (const control of Object.keys(this.empForm.controls)) {
         this.empForm.controls[control].markAsTouched();
+        this.empForm.controls[control].markAsDirty();
       }
-      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Please fill the required fields',sticky: true  });
-    }
-    this.submitted = true;
-
-    console.log(this.empForm.value);
-    const diffTime = Math.abs(
-      this.empForm.value.ToDate.getTime() -
-        this.empForm.value.FromDate.getTime()
-    );
-
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    console.log(diffDays);
-
-    this.leaveTypeId = this.empForm.value.leaveTypeId.id;
-    this.FromDate = formatDate(
-      this.empForm.value.FromDate,
-      'YYYY-MM-dd',
-      this.local
-    );
-    this.ToDate = formatDate(
-      this.empForm.value.ToDate,
-      'YYYY-MM-dd',
-      this.local
-    );
-    this.StatusId = this.empForm.value.StatusId;
-    this.Comments = this.empForm.value.Comments;
-
-    this._leaveservice
-      .applyleave(
-        this.leaveTypeId,
-        this.FromDate,
-        this.ToDate,
-        this.StatusId,
-        this.Comments,
-        diffDays
-      )
-      .subscribe((res: any) => {
-        // console.log(res);
-        if (res) {
-          this.messageService.add({
-            severity: 'success',
-            summary: 'Success',
-            detail: 'Leave details saved',
-            
-          });
-        } 
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Please fill the required fields',
+        sticky: true,
       });
+      return;
+    } else if (this.empForm.valid) {
+      this.submitted = true;
+      const diffTime = Math.abs(
+        this.empForm.value.ToDate.getTime() -
+          this.empForm.value.FromDate.getTime()
+      );
+
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+      this.leaveTypeId = this.empForm.value.leaveTypeId.id;
+      this.FromDate = formatDate(
+        this.empForm.value.FromDate,
+        'YYYY-MM-dd',
+        this.local
+      );
+      this.ToDate = formatDate(
+        this.empForm.value.ToDate,
+        'YYYY-MM-dd',
+        this.local
+      );
+      this.StatusId = this.empForm.value.StatusId;
+      this.Comments = this.empForm.value.Comments;
+
+      this._leaveservice
+        .applyleave(
+          this.leaveTypeId,
+          this.FromDate,
+          this.ToDate,
+          this.StatusId,
+          this.Comments,
+          diffDays
+        )
+        .subscribe((res: any) => {
+          // console.log(res);
+          if (res) {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Success',
+              detail: 'Leave details saved',
+            });
+          }
+        });
+    }
     this.empForm.reset();
     this.route.navigate(['/navbar/apply']);
   }
