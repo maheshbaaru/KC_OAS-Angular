feba4121363164baa24a028db3a93ca900194acb
apsar-sk <sapsar@kodecorp.com>
2023-05-23 15:55:03 +0530

    updateprofile isActive bind and appleaves,(apsar).

--- src/app/routing/Leaves/create-leave/create-leave.component.ts
+++ src/app/routing/Leaves/create-leave/create-leave.component.ts
@@ -69,46 +69,59 @@ export class CreateLeaveComponent {
     } else if (this.empForm.valid) {
       this.submitted = true;
       const diffTime = Math.abs(
-        this.empForm.value.ToDate.getTime() -
-          this.empForm.value.FromDate.getTime()
+        this.empForm.value.FromDate.getTime() -
+          this.empForm.value.ToDate.getTime()
       );
-
-      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-      this.leaveTypeId = this.empForm.value.leaveTypeId.id;
-      this.FromDate = formatDate(
-        this.empForm.value.FromDate,
-        'YYYY-MM-dd',
-        this.local
-      );
-      this.ToDate = formatDate(
-        this.empForm.value.ToDate,
-        'YYYY-MM-dd',
-        this.local
-      );
-      this.StatusId = this.empForm.value.StatusId;
-      this.Comments = this.empForm.value.Comments;
-
-      this._leaveservice
-        .applyleave(
-          this.leaveTypeId,
-          this.FromDate,
-          this.ToDate,
-          this.StatusId,
-          this.Comments,
-          diffDays
-        )
-        .subscribe((res: any) => {
-          // console.log(res);
-          if (res) {
-            this.messageService.add({
-              severity: 'success',
-              summary: 'Success',
-              detail: 'Leave details saved',
-            });
-          }
+      if (
+        Date.parse(this.empForm.value.FromDate) >
+        Date.parse(this.empForm.value.ToDate)
+      ) {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'From Date is Greater Than To Date',
+          sticky: true,
         });
+      } else {
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        console.log(diffDays);
+        this.leaveTypeId = this.empForm.value.leaveTypeId.id;
+        this.FromDate = formatDate(
+          this.empForm.value.FromDate,
+          'YYYY-MM-dd',
+          this.local
+        );
+        this.ToDate = formatDate(
+          this.empForm.value.ToDate,
+          'YYYY-MM-dd',
+          this.local
+        );
+
+        this.StatusId = this.empForm.value.StatusId;
+        this.Comments = this.empForm.value.Comments;
+
+        this._leaveservice
+          .applyleave(
+            this.leaveTypeId,
+            this.FromDate,
+            this.ToDate,
+            this.StatusId,
+            this.Comments,
+            diffDays
+          )
+          .subscribe((res: any) => {
+            // console.log(res);
+            if (res) {
+              this.messageService.add({
+                severity: 'success',
+                summary: 'Success',
+                detail: 'Leave details saved',
+              });
+            }
+          });
+        this.empForm.reset();
+        // this.route.navigate(['/navbar/apply']);
+      }
     }
-    this.empForm.reset();
-    this.route.navigate(['/navbar/apply']);
   }
 }
--- src/app/services/EmployeesDataService.ts
+++ src/app/services/EmployeesDataService.ts
@@ -12,35 +12,37 @@ const httpOptions = {
 export class EmployeedDataService {
   constructor(private http: HttpClient) {}
   getEmployeeList() {
-    
     return this.http.get<any>(
       environment.API_URL + environment.BASE_URL + 'GetEmployees',
       httpOptions
     );
   }
-  CreatenewSalaryDetails(Eid:number,Salary: number,LastRevisedDate:Date,NextRevisedDate:Date) {
-   return this.http
-      .post(
-        `https://localhost:7236/createNewSalary?EmpId=${Eid}&Salary=${Salary}&LastRevisedDate=${LastRevisedDate}&NextRevisedDate=${NextRevisedDate}`,
-       
-        httpOptions
-      )
-    
+  CreatenewSalaryDetails(
+    Eid: number,
+    Salary: number,
+    LastRevisedDate: Date,
+    NextRevisedDate: Date
+  ) {
+    return this.http.post(
+      `https://localhost:7236/createNewSalary?EmpId=${Eid}&Salary=${Salary}&LastRevisedDate=${LastRevisedDate}&NextRevisedDate=${NextRevisedDate}`,
+
+      httpOptions
+    );
   }
 
-
-
-  updateprofile(updateData: any,id:any){
-
+  updateprofile(updateData: any, id: any) {
     const body = JSON.stringify(updateData);
-    return this.http.put(`https://localhost:7236/UpdateEmployeeTbl?Id=${id}&PanNumber=${updateData.panNumber}
+    return this.http.put(
+      `https://localhost:7236/UpdateEmployeeTbl?Id=${id}&PanNumber=${updateData.panNumber}
     &FirstName=${updateData.firstName}
     &LastName=${updateData.lastName}
-    &Email=${updateData.email}&EmployeeId=${updateData.employeeId}`,
-    httpOptions);
+    &Email=${updateData.email}&EmployeeId=${updateData.employeeId}&IsActive=${updateData.isActive}`,
+      httpOptions
+    );
   }
-  getEmployeeById(id:any) {
-    return this.http.get<any>(`https://localhost:7236/GetEmployeeById?Id=${id}`,
+  getEmployeeById(id: any) {
+    return this.http.get<any>(
+      `https://localhost:7236/GetEmployeeById?Id=${id}`,
       httpOptions
     );
   }
