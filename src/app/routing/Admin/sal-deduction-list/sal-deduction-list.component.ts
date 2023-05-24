71103d0bba0ffb403230b7dedc9f52eac5a5ab7d
apsar-sk <sapsar@kodecorp.com>
2023-05-24 16:15:15 +0530

    sal-deduction& create leaves EmpID prefix(apsar)

--- src/app/routing/Admin/sal-deduction-list/sal-deduction-list.component.ts
+++ src/app/routing/Admin/sal-deduction-list/sal-deduction-list.component.ts
@@ -32,6 +32,23 @@ export class SalDeductionListComponent {
 
     this.employeeSer.getEmployee(id).subscribe((data: any) => {
       this.employeeList = data;
+
+      const salDeduction = this.employeeList.map((person: any) => ({
+        ...person,
+        empId:
+          person.empId < 10
+            ? 'KC00' + person.empId
+            : person.empId && person.empId < 100
+            ? 'KC0' + person.empId
+            : person.empId && person.employeeId >= 100
+            ? 'KC' + person.employeeId
+            : person.employeeId,
+
+        Name: `${person.firstName ? person.firstName : ''} ${
+          person.lastName ? person.lastName : ''
+        }`,
+      }));
+      this.employeeList = salDeduction;
     });
   }
 }
